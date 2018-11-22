3b50d47ce1ab22c58a56c15f7de963cd2e8c3c74
Bettina Boon Falleur <bettina.boonfalleur@gmail.com>
2018-11-22 17:30:11 +0100

    mister cocktail day 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+require 'faker'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+json = JSON.parse(ingredient_serialized)
+ingredients = json["drinks"]
+
+ingredients.each do |ingredient|
+  Ingredient.create(
+    name: ingredient["strIngredient1"]
+  )
+end
+
+puts 'Creating 5 fake cocktails...'
+5.times do
+  Cocktail.create!(
+  name: "\"The #{Faker::Hipster.word}\"",
+  )
+end
+puts 'Finished!'
